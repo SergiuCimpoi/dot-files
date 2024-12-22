678d5be5939ae454e47216e84011460b668ae8fa
Sergiu Cimpoi <sergiu.cimpoi@gmail.com>
2024-12-22 15:47:28 +0100

    added treesj

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
   "christoomey/vim-tmux-navigator",
   "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+  { "stevearc/dressing.nvim", event = "VeryLazy" },
   { "folke/todo-comments.nvim", event = "VimEnter", dependencies = { "nvim-lua/plenary.nvim" }, opts = { signs = false } },
   "tpope/vim-obsession",
 }
