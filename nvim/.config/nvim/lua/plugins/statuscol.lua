5a50d90b34f743f8b47be9e3b520808796264ce5
Sergiu Cimpoi <sergiu.cimpoi@gmail.com>
2024-12-29 19:43:26 +0100

    fixed folding in status column

--- nvim/.config/nvim/lua/plugins/statuscol.lua
+++ nvim/.config/nvim/lua/plugins/statuscol.lua
@@ -1,6 +1,17 @@
 return {
   "luukvbaal/statuscol.nvim",
   config = function()
-    require("statuscol").setup({})
+    local builtin = require("statuscol.builtin")
+    require("statuscol").setup({
+      segments = {
+        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+        { text = { "%s" }, click = "v:lua.ScSa" },
+        {
+          text = { builtin.lnumfunc, " " },
+          condition = { true, builtin.not_empty },
+          click = "v:lua.ScLa",
+        },
+      },
+    })
   end,
 }
