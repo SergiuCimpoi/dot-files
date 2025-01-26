d9d8903734592fcea3cde23d354b18475f32db46
Sergiu Cimpoi <sergiu.cimpoi@gmail.com>
2025-01-26 01:01:35 +0100

    gdb replaces lldb

---
+++ nvim/.config/nvim/lua/config/debug/lldb.lua
@@ -0,0 +1,27 @@
+return {
+  adapter = {
+    type = "server",
+    port = "${port}",
+    executable = {
+      command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+      args = { "--port", "${port}" },
+      initCommands = {
+        "settings set target.prefer-dynamic-values yes",
+        "settings set target.enable-synthetic-value true",
+      },
+    },
+  },
+  config = {
+    {
+      name = "Launch file",
+      type = "codelldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      console = "integratedTerminal",
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+    },
+  },
+}
