d3af47ce3e8b0674264a6cb8e2a606948e8c24ac
Sergiu Cimpoi <sergiu.cimpoi@gmail.com>
2025-01-26 02:03:29 +0100

    better hover

--- nvim/.config/nvim/lua/plugins/debug.lua
+++ nvim/.config/nvim/lua/plugins/debug.lua
@@ -111,31 +111,6 @@ return {
         },
       },
     })
-    -- dap.adapters.codelldb = {
-    --   type = "server",
-    --   port = "${port}",
-    --   executable = {
-    --     command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
-    --     args = { "--port", "${port}" },
-    --     initCommands = {
-    --       "settings set target.prefer-dynamic-values yes",
-    --       "settings set target.enable-synthetic-value true",
-    --     },
-    --   },
-    -- }
-    -- dap.configurations.cpp = {
-    --   {
-    --     name = "Launch file",
-    --     type = "codelldb",
-    --     request = "launch",
-    --     program = function()
-    --       return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-    --     end,
-    --     console = "integratedTerminal",
-    --     cwd = "${workspaceFolder}",
-    --     stopOnEntry = false,
-    --   },
-    -- }
 
     -- debuggers
     local gdb = require("config.debug.gdb")
