5e9a3171906c82c01906159d8e5e87c3c2f1efde
Sergiu Cimpoi <sergiu.cimpoi@gmail.com>
2025-02-02 23:16:58 +0100

    fixwed

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,7 +1,6 @@
 return {
   "christoomey/vim-tmux-navigator",
   "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-  { "stevearc/dressing.nvim", event = "VeryLazy" },
   { "folke/todo-comments.nvim", event = "VimEnter", dependencies = { "nvim-lua/plenary.nvim" }, opts = { signs = false } },
   {
     "xiyaowong/transparent.nvim",
