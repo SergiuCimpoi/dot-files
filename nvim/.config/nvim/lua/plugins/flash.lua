b694e7fe0c292e7138e66c6c60b5a85bc4208199
SergiuCimpoi <sergiu.cimpoi@gmail.com>
2025-05-12 11:05:54 +0200

    flash can be toggled with <leader>ft

---
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,62 @@
+local flash_enabled = true
+
+function ToggleFlash()
+  if flash_enabled then
+    vim.keymap.del("n", "s")
+
+    vim.keymap.del("x", "s")
+    vim.keymap.del("o", "s")
+    vim.keymap.del("n", "S")
+
+    vim.keymap.del("o", "r")
+    vim.keymap.del("o", "R")
+    vim.keymap.del("x", "R")
+    flash_enabled = false
+    vim.notify("Flash.nvim disabled", vim.log.levels.INFO)
+  else
+    vim.keymap.set({ "n", "x", "o" }, "s", function()
+      require("flash").jump()
+    end, { desc = "Flash" })
+    vim.keymap.set("n", "S", function()
+      require("flash").treesitter()
+    end, { desc = "Flash Treesitter" })
+    vim.keymap.set("o", "r", function()
+      require("flash").remote()
+    end, { desc = "Remote Flash" })
+    vim.keymap.set({ "o", "x" }, "R", function()
+      require("flash").treesitter_search()
+    end, { desc = "Treesitter Search" })
+    flash_enabled = true
+    vim.notify("Flash.nvim enabled", vim.log.levels.INFO)
+  end
+end
+
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    jump = {
+      autojump = true,
+      pos = "start",
+    },
+    modes = {
+      char = {
+        jump_labels = true,
+        multi_line = false,
+      },
+      search = {
+        mode = "search",
+      },
+    },
+  },
+    -- stylua: ignore
+    keys = {
+      { "s",          mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",          mode = { "n" },           function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",          mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",          mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      -- Toggle mapping:
+      { "<leader>tf", mode = "n",               function() ToggleFlash() end,                        desc = "Toggle Flash.nvim mappings" },
+      -- { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
+}
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -48,33 +48,6 @@ return {
     end,
   },
   {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    opts = {
-      jump = {
-        autojump = true,
-        pos = "start",
-      },
-      modes = {
-        char = {
-          jump_labels = true,
-          multi_line = false,
-        },
-        search = {
-          mode = "search",
-        },
-      },
-    },
-    -- stylua: ignore
-    keys = {
-      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "S",     mode = { "n" },           function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-    },
-  },
-  {
     "mbbill/undotree",
     config = function()
       vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle UndoTree" })
