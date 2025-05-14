0f85aa2f78992f8af9b7acfb51d296b89ba6d2b3
SergiuCimpoi <sergiu.cimpoi@gmail.com>
2025-05-14 08:46:29 +0200

    flash config fixed

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -64,3 +64,51 @@ function ToggleFlash()
     vim.notify("Flash.nvim enabled", vim.log.levels.INFO)
   end
 end
+
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = flash_opts,
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Treesitter Search",
+    },
+    {
+      "<leader>jt",
+      mode = "n",
+      function()
+        ToggleFlash()
+      end,
+      desc = "Toggle Flash.nvim mappings",
+    },
+  },
+}
