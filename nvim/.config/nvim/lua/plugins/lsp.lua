4b31575e0aa843e9171dbb2f77674478cd7cd3b5
SergiuCimpoi <sergiu.cimpoi@gmail.com>
2025-06-02 09:50:48 +0200

    lsp and cmp updates

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -59,7 +59,7 @@ return {
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         ---@diagnostic disable-next-line: missing-parameter, param-type-mismatch
-        if client and vim.lsp.protocol and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+        if client and vim.lsp.protocol and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
           local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
           vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
             buffer = event.buf,
@@ -83,7 +83,7 @@ return {
         end
 
         ---@diagnostic disable-next-line: missing-parameter, param-type-mismatch
-        if client and vim.lsp.protocol and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+        if client and vim.lsp.protocol and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
           map("<leader>th", function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
           end, "[T]oggle Inlay [H]ints")
