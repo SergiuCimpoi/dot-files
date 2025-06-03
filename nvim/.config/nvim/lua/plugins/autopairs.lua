c6de889a965f6ad3442c1b4a0650b5c455a0cf02
SergiuCimpoi <sergiu.cimpoi@gmail.com>
2025-06-03 12:08:37 +0200

    bring back autopairs

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -8,7 +8,7 @@ return {
     local nrule = require("nvim-autopairs.rule")
     local ncond = require("nvim-autopairs.conds")
     npairs.setup({})
-    npairs.add_rule(nrule("<", ">", { "rust", "cpp" }):with_pair(ncond.not_before_regex_check(" ")))
+    npairs.add_rule(nrule("<", ">", { "rust", "cpp", "typescript", "typescriptreact" }):with_pair(ncond.not_before_regex_check(" ")))
     -- If you want to automatically add `(` after selecting a function or method
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
     local cmp = require("cmp")
