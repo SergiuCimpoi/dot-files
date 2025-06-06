f1923c16d8f19210e44caf5d67ba0972ba65fbfd
Sergiu Cimpoi <sergiu.cimpoi@gmail.com>
2025-06-06 23:08:02 +0200

    auto session load at start

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -5,7 +5,7 @@ return {
     local auto_session = require("auto-session")
 
     auto_session.setup({
-      auto_restore = false,
+      -- auto_restore = false,
       suppressed_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
     })
 
