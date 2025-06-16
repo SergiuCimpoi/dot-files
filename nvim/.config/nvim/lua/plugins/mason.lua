1bb72576eaa775ec16f49b282bdc0ab25ec77363
SergiuCimpoi <sergiu.cimpoi@gmail.com>
2025-06-16 08:58:19 +0200

    back to vtsls

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,7 +1,8 @@
 return {
   "mason-org/mason-lspconfig.nvim",
   opts = {
-    ensure_installed = { "clangd", "lua_ls", "ts_ls", "eslint" },
+    -- ensure_installed = { "clangd", "lua_ls", "ts_ls", "eslint" },
+    ensure_installed = { "clangd", "lua_ls", "vtsls", "eslint" },
   },
   dependencies = {
     { "mason-org/mason.nvim", opts = {} },
