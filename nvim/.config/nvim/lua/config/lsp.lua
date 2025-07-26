e51627f7721c6f59f69c17d33bf210066d05e396
Sergiu Cimpoi <sergiu.cimpoi@gmail.com>
2025-07-26 20:21:41 +0200

    back to ts tools

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,4 +1,4 @@
-vim.lsp.enable({ "clangd", "lua_ls", "vtsls", "eslint", "hls" })
+vim.lsp.enable({ "clangd", "lua_ls", "eslint", "hls" })
 
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
