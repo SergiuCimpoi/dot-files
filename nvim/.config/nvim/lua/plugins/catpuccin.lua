2ab9e3fc7cdc46bb859c8ab01e0ff71d09739ad8
Sergiu Cimpoi <sergiu.cimpoi@gmail.com>
2026-03-30 22:19:43 +0200

    update nvim colors

--- nvim/.config/nvim/lua/plugins/catpuccin.lua
+++ nvim/.config/nvim/lua/plugins/catpuccin.lua
@@ -39,7 +39,7 @@ return {
         -- Add or remove integrations as you use them
       },
       -- Optional: Other Catppuccin options
-      transparent_background = false,
+      transparent_background = true,
       show_end_of_buffer = false,
       term_colors = false,
       no_italic = false,
