c09d32593937f5609b8c1b9d5e1f051a2c763d5e
Sergiu Cimpoi <sergiu.cimpoi@gmail.com>
2026-04-08 22:11:44 +0200

    better flash nvim

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -1,74 +1,8 @@
-local flash_opts = {
-  jump = {
-    autojump = true,
-    pos = "start",
-  },
-  modes = {
-    char = {
-      enabled = true,
-      jump_labels = true,
-      multi_line = false,
-    },
-    search = {
-      mode = "search",
-    },
-  },
-}
-
-local flash_enabled = true
-
-local function remove_flash_char_maps()
-  for _, key in ipairs({ "f", "F", "t", "T" }) do
-    pcall(vim.keymap.del, "n", key)
-    pcall(vim.keymap.del, "x", key)
-    pcall(vim.keymap.del, "o", key)
-  end
-end
-
-local function restore_flash_char_maps()
-  -- This is a workaround: reload flash.nvim's char mode mappings
-  -- The cleanest way is to re-require the plugin and call setup again
-  require("flash").setup(flash_opts)
-end
-
-function ToggleFlash()
-  local flash = require("flash")
-  if flash_enabled then
-    -- Remove char mode and custom keymaps
-    remove_flash_char_maps()
-    vim.keymap.del("n", "s")
-    vim.keymap.del("x", "s")
-    vim.keymap.del("o", "s")
-    vim.keymap.del("n", "S")
-    vim.keymap.del("o", "r")
-    vim.keymap.del("o", "R")
-    vim.keymap.del("x", "R")
-    flash_enabled = false
-    vim.notify("Flash.nvim disabled", vim.log.levels.INFO)
-  else
-    -- Restore all mappings
-    restore_flash_char_maps()
-    vim.keymap.set({ "n", "x", "o" }, "s", function()
-      flash.jump()
-    end, { desc = "Flash" })
-    vim.keymap.set("n", "S", function()
-      flash.treesitter()
-    end, { desc = "Flash Treesitter" })
-    -- vim.keymap.set("o", "r", function()
-    --   flash.remote()
-    -- end, { desc = "Remote Flash" })
-    vim.keymap.set({ "o", "x" }, "R", function()
-      flash.treesitter_search()
-    end, { desc = "Treesitter Search" })
-    flash_enabled = true
-    vim.notify("Flash.nvim enabled", vim.log.levels.INFO)
-  end
-end
-
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  opts = flash_opts,
+  ---@type Flash.Config
+  opts = {},
   keys = {
     {
       "s",
@@ -80,7 +14,7 @@ return {
     },
     {
       "S",
-      mode = { "n" },
+      mode = { "n", "x", "o" },
       function()
         require("flash").treesitter()
       end,
@@ -103,12 +37,12 @@ return {
       desc = "Treesitter Search",
     },
     {
-      "<leader>jt",
-      mode = "n",
+      "<c-j>",
+      mode = { "c" },
       function()
-        ToggleFlash()
+        require("flash").toggle()
       end,
-      desc = "Toggle Flash.nvim mappings",
+      desc = "Toggle Flash Search",
     },
   },
 }
